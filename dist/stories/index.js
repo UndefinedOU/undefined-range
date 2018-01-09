1a50e417f955813a379bfc584f74731617c2c15a
EragonJ <eragonj@eragonj.me>
2018-01-09 10:20:27 +0800

    removed some crafts

--- dist/stories/index.js
+++ dist/stories/index.js
@@ -1,3 +1,8 @@
+var _templateObject = _taggedTemplateLiteral(['\n  width: 200px;\n  height: 200px;\n'], ['\n  width: 200px;\n  height: 200px;\n']),
+    _templateObject2 = _taggedTemplateLiteral(['\n  width: 48px;\n'], ['\n  width: 48px;\n']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
@@ -5,21 +10,96 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import styled from 'styled-components';
 
-storiesOf('Welcome', module).add('to Storybook', function () {
-  return React.createElement(Welcome, { showApp: linkTo('Button') });
+import { Range, RangeGroup } from '../components';
+
+import { Input as UndefinedInput } from 'undefined-inputs';
+
+var Container = styled.div(_templateObject);
+
+var Input = styled(UndefinedInput)(_templateObject2);
+
+storiesOf('Range (Horizontal)', module).add('normal use case', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(Range, { min: 0, max: 100, value: 50 })
+  );
 });
 
-storiesOf('Button', module).add('with text', function () {
+storiesOf('Range (Vertical)', module).add('normal use case', function () {
   return React.createElement(
-    Button,
-    { onClick: action('clicked') },
-    'Hello Button'
+    Container,
+    null,
+    React.createElement(Range, { min: 0, max: 100, value: 50, direction: 'vertical' })
   );
-}).add('with some emoji', function () {
+});
+
+storiesOf('RangeGroup (Horizontal)', module).add('with Range only', function () {
   return React.createElement(
-    Button,
-    { onClick: action('clicked') },
-    '\uD83D\uDE00 \uD83D\uDE0E \uD83D\uDC4D \uD83D\uDCAF'
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 100 },
+      React.createElement(Range, { min: 0, max: 200 })
+    )
+  );
+}).add('with Input and Range (Input first)', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 80 },
+      React.createElement(Input, { max: 200, type: 'number' }),
+      React.createElement(Range, { min: 0, max: 150 })
+    )
+  );
+}).add('with Input and Range (Input last)', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 80 },
+      React.createElement(Range, { min: 0, max: 150 }),
+      React.createElement(Input, { max: 200, type: 'number' })
+    )
+  );
+});
+
+storiesOf('RangeGroup (Vertical)', module).add('with Range only', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 100, direction: 'vertical' },
+      React.createElement(Range, { min: 0, max: 200 })
+    )
+  );
+}).add('with Input and Range (Input first)', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 80, direction: 'vertical' },
+      React.createElement(Input, { max: 200, type: 'number' }),
+      React.createElement(Range, { min: 0, max: 150 })
+    )
+  );
+}).add('with Input and Range (Input last)', function () {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      RangeGroup,
+      { value: 80, direction: 'vertical' },
+      React.createElement(Range, { min: 0, max: 150 }),
+      React.createElement(Input, { max: 200, type: 'number' })
+    )
   );
 });
